5ed0294735b15788854b1821e24bc51d8eab0500
Your Name <samanbatool08@gmail.com>
2020-07-23 17:23:53 -0400

    projects page running

--- portfolio-site/src/components/projects.js
+++ portfolio-site/src/components/projects.js
@@ -9,16 +9,94 @@ class Project extends Component {
     toggleCategories() {
     if(this.state.activeTab === 0){
         return(
-            <div>
-                <h1>hitting react</h1>
-                <Card shadow={5} style={{minWidth: '450', margin: 'aut0'}}>
-                    <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://www.andreasreiterer.at/wp-content/uploads/2017/11/react-logo-825x510.jpg) center / cover'}}>React Project # 1</CardTitle>
-                        <CardText>Some dummy text project summary goes here</CardText>
+            <div className='projects-grid'>
+                {/* Project 1 */}
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background: 'url(/weather.png) center / cover'}}>Weather I should go?</CardTitle>
+                        <CardText>Comparing global weather conditions to realtime activities.
+                                <br></br>
+                                <br></br>
+                                <strong>Tech:</strong> Javascript, React, Redux, Ruby on Rails, Google Maps, OpenWeather API, Heroku, Bootstrap
+
+                        </CardText>
                         <CardActions border>
+
+                            <a href='http://google.com' target="_blank">
+                            <Button colored>Website</Button>
+                            </a>
+
+                            <a href='http://github.com' target="_blank">
                             <Button colored>Github</Button>
+                            </a>
+
+                            <a href='http://youtube.com' target="_blank">
+                            <Button colored>Demo</Button>
+                            </a>
 
                         </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name='share' />
+                        </CardMenu>
                 </Card>
+
+                     {/* Project 2 */}
+                     <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background: 'url(/painterly.png) center / cover'}}>Painterly</CardTitle>
+                        <CardText>Create your own drawing journal and organize them into different collections to reflect or finish later.
+                                <br></br>
+                                <br></br>
+                                <strong>Tech:</strong> Javascript, React, Hooks, Ruby on Rails, ActiveRecord, Heroku
+
+                        </CardText>
+                        <CardActions border>
+
+                            <a href='http://google.com' target="_blank">
+                            <Button colored>Website</Button>
+                            </a>
+
+                            <a href='http://github.com' target="_blank">
+                            <Button colored>Github</Button>
+                            </a>
+
+                            <a href='http://youtube.com' target="_blank">
+                            <Button colored>Demo</Button>
+                            </a>
+
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name='share' />
+                        </CardMenu>
+                </Card>
+
+                   {/* Project 3 */}
+                   <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background: 'url(/boggle.png) center / cover'}}>Boggle</CardTitle>
+                        <CardText>Enjoy a game of boggle using valid words rangings from 3 letters or more.
+                                <br></br>
+                                <br></br>
+                                <strong>Tech:</strong> Javascript, Ruby on Rails, PostgreSQL, Heroku, Bootstrap
+                        </CardText>
+                        <CardActions border>
+
+                            <a href='http://google.com' target="_blank">
+                            <Button colored>Website</Button>
+                            </a>
+
+                            <a href='http://github.com' target="_blank">
+                            <Button colored>Github</Button>
+                            </a>
+
+                            <a href='http://youtube.com' target="_blank">
+                            <Button colored>Demo</Button>
+                            </a>
+
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name='share' />
+                        </CardMenu>
+                </Card>
+
+                
             </div>
         )       
     }
@@ -32,15 +110,13 @@ class Project extends Component {
                         <Tab>React</Tab>
                     </Tabs>
 
-                    <section className="projects-grid">
-                        <Grid className='projects-grid'>
+                        <Grid>
                             <Cell col={12}>
                                 <div className="content">
                                 {this.toggleCategories()}
                                 </div>
                             </Cell>
                         </Grid>
-                    </section>
                 </div>
             </div>
         )
