3b353b20254c47a9aedf7b0993dee77068befbdf
Your Name <samanbatool08@gmail.com>
2020-07-23 12:48:59 -0400

    routes linked to navbar

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from './components/main';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
@@ -10,10 +11,10 @@ function App() {
     <Layout fixedHeader>
         <Header title={<span><span style={{ color: '#ddd' }}>Area / </span><strong>The Title</strong></span>}>
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
         <Drawer title="Title">
